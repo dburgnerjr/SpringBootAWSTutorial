2e6ea4266df2169a58bc2641462bedcf1eaf325f
Daniel Burgner <dburgnerjr@yahoo.com>
2022-07-01 22:09:23 -0500

    Spring Initializr Init
    Created Spring Boot application via https://start.spring.io using Spring JPA, Data and H2.

---
+++ springbootawstutorial/src/main/java/com/danielburgnerjr/springbootawstutorial/SpringbootawstutorialApplication.java
@@ -0,0 +1,13 @@
+package com.danielburgnerjr.springbootawstutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootawstutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootawstutorialApplication.class, args);
+	}
+
+}
