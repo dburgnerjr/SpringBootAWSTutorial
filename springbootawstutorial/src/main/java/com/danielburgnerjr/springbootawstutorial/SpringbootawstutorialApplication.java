94dc6f958664c3ce92caf4cae1bfa34cf910363c
Daniel Burgner <dburgnerjr@yahoo.com>
2022-07-17 12:18:31 -0500

    Built out JSP and reconfigured controller
    Built out JSP and reconfigured controller to display JSP file

--- springbootawstutorial/src/main/java/com/danielburgnerjr/springbootawstutorial/SpringbootawstutorialApplication.java
+++ springbootawstutorial/src/main/java/com/danielburgnerjr/springbootawstutorial/SpringbootawstutorialApplication.java
@@ -2,12 +2,16 @@ package com.danielburgnerjr.springbootawstutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringbootawstutorialApplication {
 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootawstutorialApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootawstutorialApplication.class, args);
 	}
-
 }
--- springbootawstutorial/src/main/java/com/danielburgnerjr/springbootawstutorial/controller/MainController.java
+++ springbootawstutorial/src/main/java/com/danielburgnerjr/springbootawstutorial/controller/MainController.java
@@ -1,13 +1,21 @@
 package com.danielburgnerjr.springbootawstutorial.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
-@RestController
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
 
+	// inject via application.properties
+	@Value("${welcome.message:test}")
+	private String message = "This is a test of the Spring Boot AWS Tutorial app.  This is only a test.";
+
 	@GetMapping("/")
-	public String welcome() {
-	  return "This is a test of the Spring Boot AWS Tutorial app.  This is only a test.";
+	public String welcome(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "welcome";
 	}
 }
