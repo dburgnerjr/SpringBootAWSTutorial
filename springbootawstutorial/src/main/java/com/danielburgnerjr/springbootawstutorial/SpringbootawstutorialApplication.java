e5983ca2a4ddbd5e6ae07ffba7561834ef84515b
Daniel Burgner <dburgnerjr@yahoo.com>
2022-08-01 16:45:05 -0500

    Reverted to simple test message.
    Reverted to controller displaying a simple text message.

--- springbootawstutorial/src/main/java/com/danielburgnerjr/springbootawstutorial/SpringbootawstutorialApplication.java
+++ springbootawstutorial/src/main/java/com/danielburgnerjr/springbootawstutorial/SpringbootawstutorialApplication.java
@@ -2,14 +2,14 @@ package com.danielburgnerjr.springbootawstutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringbootawstutorialApplication {
 
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringbootawstutorialApplication.class);
-	}
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(SpringbootawstutorialApplication.class);
+//	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootawstutorialApplication.class, args);
--- springbootawstutorial/src/main/java/com/danielburgnerjr/springbootawstutorial/controller/MainController.java
+++ springbootawstutorial/src/main/java/com/danielburgnerjr/springbootawstutorial/controller/MainController.java
@@ -1,21 +1,24 @@
 package com.danielburgnerjr.springbootawstutorial.controller;
 
-import java.util.Map;
+//import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@RestController
 public class MainController {
 
 	// inject via application.properties
 	@Value("${welcome.message:test}")
 	private String message = "This is a test of the Spring Boot AWS Tutorial app.  This is only a test.";
 
+	
 	@GetMapping("/")
-	public String welcome(Map<String, Object> model) {
-		model.put("message", this.message);
-		return "welcome";
+	public String welcome() {
+		  return "This is a test of the Spring Boot AWS Tutorial app.  This is only a test.";
+//	public String welcome(Map<String, Object> model) {
+//		model.put("message", this.message);
+//		return "welcome";
 	}
 }
